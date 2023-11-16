52c7654eb2234552d798dc5a1c31052128192fb0
Tonny <corner.store59@gmail.com>
2023-11-16 10:10:38 -0600

    [FIX] Name

--- account_payment_flow/models/mp_flujo.py
+++ account_payment_flow/models/mp_flujo.py
@@ -3,9 +3,14 @@ from odoo import fields, models
 
 class MpFlujo(models.Model):
     _name = 'mp.flujo'
-    _rec_name = "codigo"
+    _rec_name = "display_name"
 
     codigo = fields.Char()
     grupo_flujo_ids = fields.Many2many(comodel_name="mp.grupo.flujo", relation="mp_flujo_grupo_rel", column1="flujo_id",
                                        column2="grupo_flujo_id")
     decripcion = fields.Text()
+    display_name = fields.Text(compute="_compute_display_name")
+
+    def _compute_display_name(self):
+        for flujo_id in self:
+            flujo_id.display_name = str(flujo_id.codigo) + str(": ") + str(flujo_id.decripcion)
